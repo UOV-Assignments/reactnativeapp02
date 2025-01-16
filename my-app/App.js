06376219f7b99cf6d9b3897296ae5ec7ceeeab98
Lasanga Dissanayaka <129869155+lasanga890@users.noreply.github.com>
2025-01-16 06:17:39 +0000

    create first flatlist

--- my-app/App.js
+++ my-app/App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { students } from './assets/data/studentdb';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <FlatList 
+      data={students}
+      keyExtractor={item=>item.id}
+      renderItem={({item})=>
+      <Text  >{item.name}</Text>}
+      />
       <StatusBar style="auto" />
     </View>
   );
